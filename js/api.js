19cf640beb896859e8654cabbb229947902cc0d1
Eventyret <minityret@gmail.com>
2017-04-09 20:39:56 +0100

    Empty api.js
    Provided empty api.js

---
+++ js/api.js
@@ -0,0 +1,5 @@
+var apifan = '' // Calling FanartApi
+var apiv = '' // Calling RadarrApi
+var apis = '' // Calling SonarrApi
+var tmdbapi = '' // Calling TheMovieDbApi
+var tvdb = '' //Calling TheTVdbApi
