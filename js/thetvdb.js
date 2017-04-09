4bb05f870db9af04256209777a47c7aa7c7907e1
Simen Daehlin <simen.dehlin@gmail.com>
2017-04-09 22:14:29 +0100

    Added thetvdb.js to test POST request and get functionality for Sonarr

---
+++ js/thetvdb.js
@@ -0,0 +1,24 @@
+// make the request to the login endpoint
+function getToken() {
+    var loginUrl = "https://api.thetvdb.com/login"
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('POST', loginUrl, true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    xhr.addEventListener('load', function () {
+        var responseObject = JSON.parse(this.response);
+        console.log(responseObject);
+        if (responseObject.token) {
+            tokenElement.innerHTML = responseObject.token;
+        } else {
+            tokenElement.innerHTML = "No token received";
+        }
+    });
+
+    var sendObject = JSON.stringify({"apikey": "' + tvdbapikey + '", "userkey": "' + tvdbuserkey + '", "username": "' + tvdbusername + '"
+    });
+
+    console.log('going to send', sendObject);
+
+    xhr.send(sendObject);
+}
