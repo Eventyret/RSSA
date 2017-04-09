9859ecd38cc24b0148049949831af53e1c6ee3f8
Eventyret <minityret@gmail.com>
2017-04-09 20:48:38 +0100

    deleted empty api.js
    Requesting user to create one themself.

--- js/api.js
+++
@@ -1,5 +0,0 @@
-var apifan = '' // Calling FanartApi
-var apiv = '' // Calling RadarrApi
-var apis = '' // Calling SonarrApi
-var tmdbapi = '' // Calling TheMovieDbApi
-var tvdb = '' //Calling TheTVdbApi
