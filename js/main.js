1eb3b89a9ff0a99523240c7423dadbbb8a47af9d
Simen Daehlin <simen.dehlin@gmail.com>
2017-04-18 21:55:24 +0100

    added mising ; ES5

--- js/main.js
+++ js/main.js
@@ -1,142 +1,142 @@
 var getData = function (url, callback) {
-  var xhr = new XMLHttpRequest()
-  xhr.open('GET', url, true)
-  xhr.responseType = 'json'
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.responseType = 'json';
   xhr.onload = function () {
-    var status = xhr.status
+    var status = xhr.status;
     if (status === 200) {
-      callback(null, xhr.response)
+      callback(null, xhr.response);
     } else {
-      callback(status)
+      callback(status);
     }
-  }
+  };
   xhr.onerror = function() {
-    var status = xhr.status
+    var status = xhr.status;
     if (status in [400, 404, 405]){
-      console.log('Request Failed with status ' + status)
+      console.log('Request Failed with status ' + status);
     }
-  }
-  xhr.send()
-}
-var MovieListLoaded = false
-var SeriersListLoaded = false
+  };
+  xhr.send();
+};
+var MovieListLoaded = false;
+var SeriersListLoaded = false;
 
 $(document).ready(() => {
   $('#searchForm').on('submit', (e) => {
-    let searchText = $('#searchText').val()
-    getMovies(searchText)
-    e.preventDefault()
-  })
+    var searchText = $('#searchText').val();
+    getMovies(searchText);
+    e.preventDefault();
+  });
 
-  let moviesandtvshows = []
+  var moviesandtvshows = [];
 
   getData(statusurl, function (err, data) {
-    if (err != null) {
-      console.log('Something went wrong: ' + err)
+    if (err !== null) {
+      console.log('Something went wrong: ' + err);
     } else {
-      var myDate = new Date(data.buildTime)
-      $('#version').html(`<i class="fa fa-info-circle"></i> Version` + ' ' + data.version + ' ' + 'Latest Build Date: ' + myDate.getDate() + '/' + (myDate.getMonth() + 1) + '/' + myDate.getFullYear())
+      var myDate = new Date(data.buildTime);
+      $('#version').html(`<i class="fa fa-info-circle"></i> Version` + ' ' + data.version + ' ' + 'Latest Build Date: ' + myDate.getDate() + '/' + (myDate.getMonth() + 1) + '/' + myDate.getFullYear());
     }
-  })
+  });
 
   getData(radarrurl, function (err, data) {
-    if (err != null) {
-      MovieListLoaded = true
+    if (err !== null) {
+      MovieListLoaded = true;
     } else {
-      moviesandtvshows.push({ 'movies': data })
-      MovieListLoaded = true
-      searchIt()
-      var max = data.length
-      var min = 0
-      var random = Math.floor(Math.random() * (max - min + 1)) + min
-      var randomID = data[random].imdbId
+      moviesandtvshows.push({ 'movies': data });
+      MovieListLoaded = true;
+      searchIt();
+      var max = data.length;
+      var min = 0;
+      var random = Math.floor(Math.random() * (max - min + 1)) + min;
+      var randomID = data[random].imdbId;
       getData('https://webservice.fanart.tv/v3/movies/' + randomID + '?api_key=' + apifan, function (err, image) {
 
-        if (err != null || !image.moviebackground) {
-          document.body.style.backgroundColor = '#3E4551'
+        if (err !== null || !image.moviebackground) {
+          document.body.style.backgroundColor = '#3E4551';
         } else {
-          max = image.moviebackground.length
-          random = Math.floor(Math.random() * (max - min + 1)) + min
+          max = image.moviebackground.length;
+          random = Math.floor(Math.random() * (max - min + 1)) + min;
           if (random >= max) {
-            random = max - 1
+            random = max - 1;
           }
-          var randomIDurl = image.moviebackground[random].url
-          $('body').css('background-image', 'url(' + randomIDurl + ')')
-          $('body').addClass('randombg')
-          sessionStorage.setItem('backgroundImageUrl', randomIDurl)
+          var randomIDurl = image.moviebackground[random].url;
+          $('body').css('background-image', 'url(' + randomIDurl + ')');
+          $('body').addClass('randombg');
+          sessionStorage.setItem('backgroundImageUrl', randomIDurl);
         }
-      })
+      });
     }
-  })
+  });
 
   getData(sonarrurl, function (err, data) {
-    if (err != null) {
-      SeriersListLoaded = true
-      console.log('Something went wrong: ' + err)
+    if (err !== null) {
+      SeriersListLoaded = true;
+      console.log('Something went wrong: ' + err);
     } else {
-      moviesandtvshows.push({ 'series': data })
-      SeriersListLoaded = true
-      searchIt()
+      moviesandtvshows.push({ 'series': data });
+      SeriersListLoaded = true;
+      searchIt();
     }
-  })
+  });
 
-  this.data = moviesandtvshows
-})
+  this.data = moviesandtvshows;
+});
 
 function searchIt () {
   if (MovieListLoaded && SeriersListLoaded) {
-    var searchFor = window.location.href.split('?q=')
-    var searchQuery = searchFor[1]
+    var searchFor = window.location.href.split('?q=');
+    var searchQuery = searchFor[1];
 
-    if (searchQuery != null && searchFor.length > 1) {
-      document.getElementById('searchText').value = decodeURI(searchQuery)
-      getMovies(searchQuery)
+    if (searchQuery !== null && searchFor.length > 1) {
+      document.getElementById('searchText').value = decodeURI(searchQuery);
+      getMovies(searchQuery);
     }
   }
 }
 
 // Loops checks if movie is in collection
 function filterMovies (id) {
-  let isDownloaded = false // Set default value of `false`
+  var isDownloaded = false;
   this.data.forEach(function (type) {
     [type].forEach(function (entry) {
       $.each(entry, function (i, item) {
         $.each(item, function (x, y) {
           if (y.imdbId === id && y.sizeOnDisk > 0) {
-            isDownloaded = true
+            isDownloaded = true;
           }
-        })
-      })
-    })
-  })
-  return isDownloaded
+        });
+      });
+    });
+  });
+  return isDownloaded;
 }
 
 // Search Function
 function getMovies (searchText) {
   getData('https://www.omdbapi.com/?s=' + searchText, function (err, response) {
-    if (err != null) {
+    if (err !== null) {
     } else {
-      let omdbData = response.Search
-      let output = ''
+      var omdbData = response.Search;
+      var output = '';
       $.each(omdbData, (index, movie) => {
-        output += htmlWriteResults(movie)
-      })
+        output += htmlWriteResults(movie);
+      });
 
-      $('#movies').html(output)
+      $('#movies').html(output);
     }
-  })
+  });
 }
 
 // Writes the Search Results
 function htmlWriteResults (cases) {
-  let myHTML = ''
+  var myHTML = '';
   myHTML += `<div class="col-md-4">
-            <div class="well text-center">`
+            <div class="well text-center">`;
   if (filterMovies(cases.imdbID)) {
-    myHTML += `<div class="alert alert-success" id="${cases.imdbID}inCollection"><i class="fa fa-cloud-download"></i> In Collection</div>`
+    myHTML += `<div class="alert alert-success" id="${cases.imdbID}inCollection"><i class="fa fa-cloud-download"></i> In Collection</div>`;
   } else {
-    myHTML += `<div class="alert alert-danger" id="${cases.imdbID}notInCollection"><i class="fa fa-exclamation-triangle"></i> Not in Collection</div>`
+    myHTML += `<div class="alert alert-danger" id="${cases.imdbID}notInCollection"><i class="fa fa-exclamation-triangle"></i> Not in Collection</div>`;
   }
   myHTML += `<img src="${posterError(cases.Poster)}">
               <h5 id="whiteheader">${cases.Title} (${cases.Year.substring(0, 4)})</h5>
@@ -145,46 +145,46 @@ function htmlWriteResults (cases) {
                 </div>
             </div>
           </div>
-          `
-  return myHTML
+          `;
+  return myHTML;
 }
 
 // Check for tvdbID
 
 function getTvdb (id) {
-  let tvdbid = ''
+  var tvdbid = '';
   this.data.forEach(function (type) {
     [type].forEach(function (entry) {
       $.each(entry, function (i, item) {
         $.each(item, function (x, y) {
           if (y.imdbId === id && y.tvdbId !== null && y.tvdbId !== '') {
-            tvdbid = y.tvdbId
+            tvdbid = y.tvdbId;
           }
-        })
-      })
-    })
-  })
-  return tvdbid
+        });
+      });
+    });
+  });
+  return tvdbid;
 }
 
 // Set storage Items
 function movieSelected (id) {
-  var tvdbID = getTvdb(id)
+  var tvdbID = getTvdb(id);
   if (tvdbID !== '') {
-    sessionStorage.setItem('tvdbID', tvdbID)
+    sessionStorage.setItem('tvdbID', tvdbID);
   }
-  sessionStorage.setItem('movieId', id)
+  sessionStorage.setItem('movieId', id);
   if (document.getElementById(id + 'inCollection')) {
-    sessionStorage.setItem('inCollection', true)
+    sessionStorage.setItem('inCollection', true);
   } else {
-    sessionStorage.setItem('inCollection', false)
-  };
-  var searchedFor = $('#searchText').val()
-  window.location = 'info.html?q=' + searchedFor
-  return false
+    sessionStorage.setItem('inCollection', false);
+  }
+  var searchedFor = $('#searchText').val();
+  window.location = 'info.html?q=' + searchedFor;
+  return false;
 }
 
 // Modal for info
 $(document).ready(function () {
-  $('#demoModal').modal('show')
-})
+  $('#demoModal').modal('show');
+});
