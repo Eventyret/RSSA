0ee00bd193e37146414b99e00efea6c33b617d51
Simen Daehlin <simen.dehlin@gmail.com>
2017-04-19 22:11:20 +0100

    updated api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-var FANARTAPI = 'ecb3947c7bf1bc176d52128a82376f85' // FanArt.tv API KEY
-var RADARRURL = './demodata/movies.json' // Your Radarr URL
-var SONARRURL = './demodata/series.json' // Your Sonarr URL
-var STATUSURL = './demodata/status.json' // Your Radarr URL with /api/system/status
+var FANARTAPI = 'ecb3947c7bf1bc176d52128a82376f85'; // FanArt.tv API KEY
+var RADARRURL = './demodata/movies.json'; // Your Radarr URL
+var SONARRURL = './demodata/series.json'; // Your Sonarr URL
+var STATUSURL = './demodata/status.json'; // Your Radarr URL with /api/system/status
