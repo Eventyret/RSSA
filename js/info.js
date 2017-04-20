ab34f0713fb9e1d9a8da1e7d36a245220da7e168
Simen Daehlin <simen.dehlin@gmail.com>
2017-04-20 19:36:19 +0100

    testing md vs lg

--- js/info.js
+++ js/info.js
@@ -60,10 +60,10 @@ function htmlWriteInfo(movie) {
   var inCollection = sessionStorage.getItem('inCollection');
   var myHTML = '';
   myHTML += `<div class="row">
-            <div class="col-md-4 center-block" id="poster">
+            <div class="col-lg-4" id="poster">
               <img src="${posterError(movie.Poster)}" class="img-thumbnail img-rounded" id="movieposter">
             </div>
-              <div class="col-md-8">
+              <div class="col-lg-8">
               <h2 class="text-center" id="movietitle">${movie.Title}</h2>
                 <ul class="list-group">
                   <li class="list-group-item"><strong><i class="fa fa-file"></i> Genre:</strong> ${movie.Genre}</li>
