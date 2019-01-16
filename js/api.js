49534701f5d15282ed146596a5fb05f80cd721ca
Simen Daehlin <simen.dehlin@gmail.com>
2019-01-16 22:04:01 +0000

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
 var FANARTAPI = 'ecb3947c7bf1bc176d52128a82376f85'; // FanArt.tv API KEY
 var RADARRURL = './demodata/movies.json'; // Your Radarr URL
 var SONARRURL = './demodata/series.json'; // Your Sonarr URL
-var OMDBAPIKEY = '292490c5'; // OMDB API KEY
+var OMDBAPIKEY = 'c4299fe0'; // OMDB API KEY
